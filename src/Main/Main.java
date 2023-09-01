3fa8f20a632779389c6360d3dab17fedf71b3d48
User <User@Z>
2023-09-01 13:10:36 -0500

    Z_Login_Ventana

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+
+package Main;
+
+import Vista.Login_Vista;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //Instancia del login para que sea la primera ventana en abrirse
+        
+        Login_Vista login = new Login_Vista();
+        
+        login.setVisible(true); 
+        
+        
+    }
+    
+    
+}
